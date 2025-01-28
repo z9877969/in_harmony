1f65c0c6a2da161a6aa0928dd951cb8c981932d2
z9877969 <z9877969@ukr.net>
2025-01-28 14:09:12 +0200

    adds linter settings

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@ import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
+  console.log("OBJECT-222");
   return (
     <div className={styles.page}>
       <main className={styles.main}>

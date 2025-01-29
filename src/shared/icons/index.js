ebacea339170cf5de6241661244013b493f22142
Oksana Tanashchuk <oksana.tanashuk@gmail.com>
2025-01-29 18:13:01 +0100

    -added sprite - added colored .svg -added re-export to index.js

--- src/shared/icons/index.js
+++ src/shared/icons/index.js
@@ -1,1 +1,8 @@
-export {default as sprite} from './sprite.svg'
+export { default as sprite } from './sprite.svg';
+export { default as ua_flag } from './flag-for-ukraine.svg';
+export { default as google_logo } from './google-logo.svg';
+export { default as inHarmony_logo } from './logo-inHarmony.svg';
+export { default as mastercard } from './mastercard.svg';
+export { default as paypal_long } from './paypal_long.svg';
+export { default as paypal_short } from './paypal_short.svg';
+export { default as visa } from './visa-logo.svg';

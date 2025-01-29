21a2d33a421b235fbb0b09287cecd7caf67df53d
Oksana Tanashchuk <oksana.tanashuk@gmail.com>
2025-01-29 18:41:33 +0100

    rename icons -added mare icons to   sprite

--- src/shared/icons/index.js
+++ src/shared/icons/index.js
@@ -3,6 +3,6 @@ export { default as uaFlag } from './flag-for-ukraine.svg';
 export { default as googleLogo } from './google-logo.svg';
 export { default as inHarmonyLogo } from './logo-inHarmony.svg';
 export { default as mastercard } from './mastercard.svg';
-export { default as paypalLong } from './paypal_long.svg';
-export { default as paypalShort } from './paypal_short.svg';
+export { default as paypalLong } from './paypal-long.svg';
+export { default as paypalShort } from './paypal-short.svg';
 export { default as visa } from './visa-logo.svg';

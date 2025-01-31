431003e00d4b15072c915fdab80bdd199c37dbb6
Yurii Makoveichuk <yuriimakoveichuk@gmail.com>
2025-01-31 18:20:39 +0200

    add RadioButton and FormWithSumButtons

--- src/modules/mainPageDonat/components/MainPageDonat/MainPageDonat.jsx
+++ src/modules/mainPageDonat/components/MainPageDonat/MainPageDonat.jsx
@@ -1,9 +1,11 @@
+import { FormWithSumButtons } from '@/shared/components/index.js';
 import s from './MainPageDonat.module.scss';
 
 const MainPageDonat = () => {
   return (
     <section>
       <h2 className={s.title}>MainPageDonat</h2>
+      <FormWithSumButtons />
     </section>
   );
 };
--- src/shared/components/index.js
+++ src/shared/components/index.js
@@ -3,3 +3,5 @@ export { default as Input } from './Input/Input';
 export { default as InputArea } from './InputArea/InputArea';
 export { default as Icon } from './Icon/Icon';
 export { default as Button } from './Button/Button';
+export { default as FormWithSumButtons } from './FormWithSumButtons/FormWithSumButtons.jsx';
+export { default as RadioButton } from './RadioButton/RadioButton.jsx';

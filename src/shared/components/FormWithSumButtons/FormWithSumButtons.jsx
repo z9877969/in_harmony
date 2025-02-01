f6579f37fb615fcc1d74d28f187aecf0a3c322d0
z9877969 <z9877969@ukr.net>
2025-02-01 21:57:52 +0200

    fixes linter and FormWithSumButtons container

--- src/shared/components/FormWithSumButtons/FormWithSumButtons.jsx
+++ src/shared/components/FormWithSumButtons/FormWithSumButtons.jsx
@@ -3,7 +3,7 @@
 import { ErrorMessage, Field, Form, Formik } from 'formik';
 import * as yup from 'yup';
 
-import { Button, Container, RadioButton } from '../index.js';
+import { Button, RadioButton } from '../index.js';
 
 import { arrowFormDonate as FormIcon } from '/public/icons';
 
@@ -21,77 +21,74 @@ const FormWithSumButtons = () => {
   const amounts = [200, 500, 1000];
 
   const onSubmit = (values, { resetForm }) => {
+    // eslint-disable-next-line
     console.log('Form donate:', values);
     resetForm();
   };
   return (
-    <>
-      <Container>
-        <div className={s.boxForm}>
-          <Formik
-            initialValues={{
-              amount: '',
-              donateTime: 'oneTime',
-            }}
-            validationSchema={validationSchemaFormDonate}
-            onSubmit={onSubmit}
-          >
-            {({ setFieldValue }) => (
-              <Form className={s.form}>
-                <Field name="donateTime">
-                  {({ field }) => (
-                    <RadioButton
-                      options={data.donateTimeOptions}
-                      name="donateTime"
-                      onChange={(value) => setFieldValue(field.name, value)}
-                    />
-                  )}
-                </Field>
-                <div className={s.textForm}>
-                  <p>{data.text}</p>
-                  <FormIcon className={s.iconForm} />
-                </div>
-                <div className={s.boxBtn}>
-                  {amounts.map((amount) => (
-                    <Button
-                      key={amount}
-                      type="button"
-                      variant="secondary"
-                      className={s.btnDonate}
-                      onClick={() => setFieldValue('amount', amount)}
-                    >
-                      +{amount} UAH
-                    </Button>
-                  ))}
-                </div>
-                <div className={s.boxInput}>
-                  <Field
-                    type="number"
-                    id="amount"
-                    name="amount"
-                    placeholder="0"
-                    className={s.inputDonate}
-                  />
-                  <span className={s.spanInput}>UAH</span>
-                  <ErrorMessage
-                    className={s.errorText}
-                    name="amount"
-                    component="span"
-                  />
-                </div>
-
-                <Button type="submit" variant="primary" className={s.btnSubmit}>
-                  {data.submitButton}
+    <div className={s.boxForm}>
+      <Formik
+        initialValues={{
+          amount: '',
+          donateTime: 'oneTime',
+        }}
+        validationSchema={validationSchemaFormDonate}
+        onSubmit={onSubmit}
+      >
+        {({ setFieldValue }) => (
+          <Form className={s.form}>
+            <Field name="donateTime">
+              {({ field }) => (
+                <RadioButton
+                  options={data.donateTimeOptions}
+                  name="donateTime"
+                  onChange={(value) => setFieldValue(field.name, value)}
+                />
+              )}
+            </Field>
+            <div className={s.textForm}>
+              <p>{data.text}</p>
+              <FormIcon className={s.iconForm} />
+            </div>
+            <div className={s.boxBtn}>
+              {amounts.map((amount) => (
+                <Button
+                  key={amount}
+                  type="button"
+                  variant="secondary"
+                  className={s.btnDonate}
+                  onClick={() => setFieldValue('amount', amount)}
+                >
+                  +{amount} UAH
                 </Button>
-              </Form>
-            )}
-          </Formik>
-          <div className={s.boxLink}>
-            <Link href="/payments">{data.otherPayment}</Link>
-          </div>
-        </div>
-      </Container>
-    </>
+              ))}
+            </div>
+            <div className={s.boxInput}>
+              <Field
+                type="number"
+                id="amount"
+                name="amount"
+                placeholder="0"
+                className={s.inputDonate}
+              />
+              <span className={s.spanInput}>UAH</span>
+              <ErrorMessage
+                className={s.errorText}
+                name="amount"
+                component="span"
+              />
+            </div>
+
+            <Button type="submit" variant="primary" className={s.btnSubmit}>
+              {data.submitButton}
+            </Button>
+          </Form>
+        )}
+      </Formik>
+      <div className={s.boxLink}>
+        <Link href="/payments">{data.otherPayment}</Link>
+      </div>
+    </div>
   );
 };
 

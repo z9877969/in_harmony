c8715bdc40b7dca8b21a15121761e704725f05c0
Elostay <nastia60203@gmail.com>
2025-02-04 15:46:08 +0200

    add togle private/public

--- src/shared/components/PublicPrivateForm/PublicPrivateForm.jsx
+++ src/shared/components/PublicPrivateForm/PublicPrivateForm.jsx
@@ -1,32 +1,53 @@
 'use client';
 import { useState } from 'react';
-import { Button, Dropdown, Input, InputArea } from '..';
+import { Button, Dropdown, Input, InputArea, RadioButton } from '..';
 import s from './PublicPrivateForm.module.scss';
-
+import data from './data/PublicPrivateForm.json';
 import Link from 'next/link';
 const PublicPrivateForm = () => {
   const [isChecked, setIseChecked] = useState(false);
+  const [isPublic, setIsPublic] = useState(true);
+
+  const haandleChange = () => {
+    setIsPublic((prev) => !prev);
+  };
   const toggleCheckbox = () => {
     setIseChecked((prev) => !prev);
   };
+
   return (
     <div className={s.container}>
-      <p className={s.title}>
-        Надихайте інших –<br /> зробіть свій внесок публічним!
-      </p>
-      <div className={s.inputContainer}>
-        <h3>TogglerComponent</h3>
-        <Input type="text" placeholder="Ім'я" name="name" className={s.input} />
-        <Input
-          type="text"
-          placeholder="Email"
-          name="email"
-          className={s.input}
-        />
+      <h2 className={s.title}>{data.title}</h2>
 
-        <InputArea placeholder="Повідомлення" style={{ padding: '16px' }} />
+      <div>
+        <RadioButton
+          options={data.donateOptions}
+          name="donateOptions"
+          onChange={haandleChange}
+        />
+        {isPublic && (
+          <div className={s.inputContainer}>
+            <Input
+              type="text"
+              placeholder={data.placeholderName}
+              name="name"
+              className={s.input}
+            />
+            <Input
+              type="text"
+              placeholder={data.placeholderEmail}
+              name="email"
+              className={s.input}
+            />
+
+            <InputArea
+              placeholder={data.placeholderMessage}
+              style={{ padding: '16px' }}
+            />
+          </div>
+        )}
       </div>
-      <p className={s.destination}>Призначення</p>
+      <p className={s.destination}>{data.destination}</p>
       <Dropdown />
       <div className={s.checkboxContainer}>
         <input
@@ -35,13 +56,13 @@ const PublicPrivateForm = () => {
           onClick={toggleCheckbox}
           type="checkbox"
         />
-        <p>Покрити комісію 2%. Без втрат для допомоги.</p>
+        <p>{data.coverCommission}</p>
       </div>
       <Button>
-        <Link href="#">До сплати</Link>
+        <Link href="#">{data.btnText}</Link>
       </Button>
       <Link href="#" className={s.payment}>
-        Інші способи оплати
+        {data.otherPaymentMethods}
       </Link>
     </div>
   );

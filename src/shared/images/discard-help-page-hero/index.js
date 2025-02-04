6a91236d5aeb845e143c054fe82a727274c47b6f
Elostay <nastia60203@gmail.com>
2025-02-04 13:48:52 +0200

    discardHelpPageHero

---
+++ src/shared/images/discard-help-page-hero/index.js
@@ -0,0 +1,6 @@
+export { default as peacePosterMobile } from './peace-poster.png';
+export { default as peacePosterMobile2x } from './peace-poster@2x.png';
+export { default as peacePosterTablet } from './peace-poster-tablet.png';
+export { default as peacePosterTablet2x } from './peace-poster-tablet@2x.png';
+export { default as peacePosterDesktop } from './peace-poster-desktop.png';
+export { default as peacePosterDesktop2x } from './peace-poster-desktop@2x.png';

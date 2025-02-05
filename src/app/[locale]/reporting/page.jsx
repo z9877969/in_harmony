e8f7bd29f2611989f296f7267b443666d3b6213a
Ella-Kadriaieva <ella.kadriaieva@gmail.com>
2025-02-05 10:07:32 +0200

    add reporting page

--- src/app/[locale]/reporting/page.jsx
+++ src/app/[locale]/reporting/page.jsx
@@ -1,12 +1,10 @@
 import { ReportingPageFilters } from '@/modules/reportingPageFilters/index.js';
 import { ReportingPageDonate } from '@/modules/reportingPageDonate';
 import { ReportingPageFinishedProjects } from '@/modules/reportingPageFinishedProjects';
-import s from './page.module.scss';
 
 const ReportingPage = () => {
   return (
     <>
-      <h1 className={s.title}>ReportingPage</h1>
       <ReportingPageFilters />
       <ReportingPageFinishedProjects />
       <ReportingPageDonate />

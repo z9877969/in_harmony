03c3e88e7abadcffe639affff83d67486bc06599
Andriy Kyrylenko <46637279+z9877969@users.noreply.github.com>
2025-02-06 17:10:05 +0200

    Update page.jsx

--- src/app/[locale]/collection/page.jsx
+++ src/app/[locale]/collection/page.jsx
@@ -5,7 +5,6 @@ import s from './page.module.scss';
 const CollectionPage = () => {
   return (
     <>
-      <h1 className={s.title}>CollectionPage</h1>
       <CollectionPageActive />
 
       <CollectionPageReporting />

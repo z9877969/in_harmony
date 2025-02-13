b5016b6513ced84db85a401feea49c18686b76fb
Pasha2407 <70815888+Pasha2407@users.noreply.github.com>
2025-02-13 14:16:34 +0100

    fix remark

--- src/modules/mainPageAbout/components/MainPageAbout/MainPageAbout.jsx
+++ src/modules/mainPageAbout/components/MainPageAbout/MainPageAbout.jsx
@@ -1,19 +1,10 @@
-'use client';
-
-import Link from 'next/link';
-import { usePathname } from 'next/navigation';
-import { useTranslation } from 'react-i18next';
-import { ROUTES } from '@/shared/constants';
 import { Container, Section } from '@/shared/components';
-import Button from '@/shared/components/Button/Button';
 import ActivitiesCard from '../ActivitiesCard/ActivitiesCard';
+import LearnMoreButton from '../LearnMoreButton/LearnMoreButton';
 
 import s from './MainPageAbout.module.scss';
 
 const MainPageAbout = ({ content }) => {
-  const pathname = usePathname();
-  const locale = pathname.split('/')[1];
-  const { t } = useTranslation('mainPage');
   const icons = ['icon-support', 'icon-hand', 'icon-animal-paw', 'icon-house'];
 
   return (
@@ -40,13 +31,7 @@ const MainPageAbout = ({ content }) => {
           </div>
           <p className={s.motivation}>{content?.motivation}</p>
           <div className={s.image}></div>
-          <div className={s.learnMore}>
-            <Link href={`/${locale}/${ROUTES.ABOUT}`}>
-              <Button variant="secondary" border={true}>
-                {t('about.learnMore')}
-              </Button>
-            </Link>
-          </div>
+          <LearnMoreButton style={s.learnMore} />
         </div>
       </Container>
     </Section>

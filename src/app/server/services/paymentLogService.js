d65b9abda337df7d534a98241a7a0d300e6281a0
Serhii Hudzenko <gudzsvmail@gmail.com>
2025-02-18 11:42:29 +0100

    add schema to PaymentLog

--- src/app/server/services/paymentLogService.js
+++ src/app/server/services/paymentLogService.js
@@ -1,13 +1,77 @@
+import { PAYMENT_CONFIG } from '@/shared/constants/index.js';
 import PaymentLogModel from '../models/PaymentLogModel/PaymentLogModel.js';
+import generateHash from '../utils/generateHash.js';
 
 export const createLogPayment = async (req, res) => {
   try {
-    const paymentLog = new PaymentLogModel(req.body);
+    const {
+      orderReference,
+      merchantAccount,
+      merchantSignature,
+      amount,
+      currency,
+      authCode,
+      email,
+      phone,
+      createdDate,
+      processingDate,
+      cardPan,
+      cardType,
+      issuerBankCountry,
+      issuerBankName,
+      recToken,
+      transactionStatus,
+      reason,
+      reasonCode,
+      fee,
+      paymentSystem,
+      acquirerBankName,
+      cardProduct,
+      clientName,
+    } = req.body;
+
+    const status = 'accept';
+    const time = Math.floor(Date.now() / 1000);
+    const { key } = PAYMENT_CONFIG;
+
+    const paymentLog = new PaymentLogModel({
+      merchantAccount,
+      merchantSignature,
+      orderReference,
+      amount,
+      currency,
+      authCode,
+      email,
+      phone,
+      createdDate,
+      processingDate,
+      cardPan,
+      cardType,
+      issuerBankCountry,
+      issuerBankName,
+      recToken,
+      transactionStatus,
+      reason,
+      reasonCode,
+      fee,
+      paymentSystem,
+      acquirerBankName,
+      cardProduct,
+      clientName,
+    });
 
     await paymentLog.save();
 
-    return res.status(201).json(paymentLog);
+    const controlString = `${orderReference};${status};${time}`;
+    const signature = generateHash(controlString, key);
+
+    return res.status(201).json({
+      orderReference,
+      status,
+      time,
+      signature,
+    });
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    return res.status(500).json({ message: error.message });
   }
 };

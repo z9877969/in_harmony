dc926462ab6d0b3e070f6438a837ff6343195efa
gudzsv <gudzsvmail@gmail.com>
2025-02-19 16:09:15 +0100

    add additional logs
    add additional logs
    
    Co-Authored-By: Andriy Kyrylenko <46637279+z9877969@users.noreply.github.com>

--- src/app/server/services/paymentLogService.js
+++ src/app/server/services/paymentLogService.js
@@ -4,67 +4,82 @@ import generateHash from '../utils/generateHash.js';
 
 export const createLogPayment = async (req, res) => {
   try {
-    const {
-      orderReference,
-      merchantAccount,
-      merchantSignature,
-      amount,
-      currency,
-      authCode,
-      email,
-      phone,
-      createdDate,
-      processingDate,
-      cardPan,
-      cardType,
-      issuerBankCountry,
-      issuerBankName,
-      recToken,
-      transactionStatus,
-      reason,
-      reasonCode,
-      fee,
-      paymentSystem,
-      acquirerBankName,
-      cardProduct,
-      clientName,
-    } = req.body;
+    // const {
+    //   orderReference,
+    //   merchantAccount,
+    //   merchantSignature,
+    //   amount,
+    //   currency,
+    //   authCode,
+    //   email,
+    //   phone,
+    //   createdDate,
+    //   processingDate,
+    //   cardPan,
+    //   cardType,
+    //   issuerBankCountry,
+    //   issuerBankName,
+    //   recToken,
+    //   transactionStatus,
+    //   reason,
+    //   reasonCode,
+    //   fee,
+    //   paymentSystem,
+    //   acquirerBankName,
+    //   cardProduct,
+    //   clientName,
+    // } = req.body;
 
     // eslint-disable-next-line no-console
-    console.log('>>>>>>>>>>> paymentLogService body: ', req.body);
+    try {
+      // eslint-disable-next-line no-console
+      console.log('Type of req.body:', typeof req.body);
+
+      const parsedBody =
+        typeof req.body === 'string' ? JSON.parse(req.body) : req.body;
+      // eslint-disable-next-line no-console
+      console.log('Parsed req.body:', parsedBody);
+    } catch (error) {
+      // eslint-disable-next-line no-console
+      console.error('Error parsing req.body:', error);
+    }
 
     const status = 'accept';
     const time = Math.floor(Date.now() / 1000);
     const { key } = PAYMENT_CONFIG;
 
-    const paymentLog = new PaymentLogModel({
-      merchantAccount,
-      merchantSignature,
-      orderReference,
-      amount,
-      currency,
-      authCode,
-      email,
-      phone,
-      createdDate,
-      processingDate,
-      cardPan,
-      cardType,
-      issuerBankCountry,
-      issuerBankName,
-      recToken,
-      transactionStatus,
-      reason,
-      reasonCode,
-      fee,
-      paymentSystem,
-      acquirerBankName,
-      cardProduct,
-      clientName,
-    });
+    // const data = {
+    //   merchantAccount,
+    //   merchantSignature,
+    //   orderReference,
+    //   amount,
+    //   currency,
+    //   authCode,
+    //   email,
+    //   phone,
+    //   createdDate,
+    //   processingDate,
+    //   cardPan,
+    //   cardType,
+    //   issuerBankCountry,
+    //   issuerBankName,
+    //   recToken,
+    //   transactionStatus,
+    //   reason,
+    //   reasonCode,
+    //   fee,
+    //   paymentSystem,
+    //   acquirerBankName,
+    //   cardProduct,
+    //   clientName,
+    // }
+
+    const paymentLog = new PaymentLogModel(req.body);
 
     await paymentLog.save();
 
+    const orderReference = 'test-order';
+    // const controlString = `${orderReference};${status};${time}`;
     const controlString = `${orderReference};${status};${time}`;
     const signature = generateHash(controlString, key);
 

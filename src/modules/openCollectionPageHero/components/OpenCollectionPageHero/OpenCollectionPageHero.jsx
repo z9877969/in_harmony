ec669d099468716bf3de78d7a5abf238ccc830ad
Elostay <nastia60203@gmail.com>
2025-02-20 17:11:20 +0200

    check data from DB + styles

--- src/modules/openCollectionPageHero/components/OpenCollectionPageHero/OpenCollectionPageHero.jsx
+++ src/modules/openCollectionPageHero/components/OpenCollectionPageHero/OpenCollectionPageHero.jsx
@@ -1,26 +1,39 @@
+'use client';
 import { Container } from '@/shared/components';
-import { Icon } from '@/shared/components';
-import sectionContent from '../../data/sectionContent.json';
 import s from './OpenCollectionPageHero.module.scss';
 import OpenCollectionProgress from '../OpenCollectionProgress/OpenCollectionProgress';
-import CollectionStatusLabel from '../CollectionStatusLabel/CollectionStatusLabel';
+import { useTranslation } from 'react-i18next';
+import Image from 'next/image';
 
 const OpenCollectionPageHero = ({ content }) => {
-  const { title, long_desc, status } = content;
-
+  const { title, long_desc, importance, image } = content;
+  const { t } = useTranslation('activeCollectionCard');
   return (
     <section className={s.wrapper}>
       {content && (
         <Container>
           <div className={s.mainContent}>
-            <div className={s.image}>
-              <Icon className={s.icon} iconName={sectionContent.icon} />
+            <div className={s.imageWrapper}>
+              {/* <div className={s.image}> */}
+              <Image
+                className={s.image}
+                src={image[0]}
+                alt={title + 'image'}
+                fill
+                priority
+                sizes="100%"
+                placeholder="blur"
+                blurDataURL="/images/blur-placeholder.jpg"
+              />
+              {/* </div> */}
             </div>
             <div className={s.info}>
               <h2 className={s.title}>{title}</h2>
-              <div className={s.status}>
-                <CollectionStatusLabel status={status} />
-              </div>
+              <p
+                className={importance === 'urgent' ? s.importance : s.nonUrgent}
+              >
+                {t(`importanceType.${importance}`)}
+              </p>
               <div className={s.description}>
                 <p>{long_desc.section1}</p>
                 <p>{long_desc.section2}</p>

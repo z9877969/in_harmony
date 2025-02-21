5badf043b3729986dc89a6c3ba98895cbd3f63ed
Klymovych Oleksandr <warvar777@gmail.com>
2025-02-21 14:56:40 +0100

    Update ContactUs.jsx

--- src/modules/footer/components/ContactUs/ContactUs.jsx
+++ src/modules/footer/components/ContactUs/ContactUs.jsx
@@ -5,14 +5,11 @@ import { useFormik } from 'formik';
 import { useTranslation } from 'react-i18next';
 import { validationSchema } from '../../validation/validationSchema.js';
 import s from './ContactUs.module.scss';
-import { useTranslation } from 'react-i18next';
 
 const ContactUs = () => {
   const initialValues = { name: '', email: '', message: '' };
 
   const { t } = useTranslation('forms');
-
-
 
   const formik = useFormik({
     initialValues,
@@ -37,9 +34,7 @@ const ContactUs = () => {
             name="name"
             value={formik.values.name}
             type="text"
-
             placeholder={t('support.placeholderName')}
-
             onChange={formik.handleChange}
             onBlur={formik.handleBlur}
             error={formik.touched.name && formik.errors.name}
@@ -50,9 +45,7 @@ const ContactUs = () => {
             name="email"
             value={formik.values.email}
             type="text"
-
             placeholder={t('support.placeholderEmail')}
-
             onChange={formik.handleChange}
             onBlur={formik.handleBlur}
             error={formik.touched.email && formik.errors.email}
@@ -62,9 +55,7 @@ const ContactUs = () => {
             className={s.textAreaFooter}
             name="message"
             value={formik.values.message}
-
             placeholder={t('support.placeholderMessage')}
-
             onChange={formik.handleChange}
             onBlur={formik.handleBlur}
             error={formik.touched.message && formik.errors.message}

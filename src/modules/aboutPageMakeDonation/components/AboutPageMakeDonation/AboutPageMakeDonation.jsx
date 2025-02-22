49c09afe910cf46bcb7771c2c79ae43a3f409009
Yurii Makoveichuk <yuriimakoveichuk@gmail.com>
2025-02-22 13:03:41 +0200

    transferred button

--- src/modules/aboutPageMakeDonation/components/AboutPageMakeDonation/AboutPageMakeDonation.jsx
+++ src/modules/aboutPageMakeDonation/components/AboutPageMakeDonation/AboutPageMakeDonation.jsx
@@ -1,16 +1,10 @@
-'use client';
-
-import { usePathname } from 'next/navigation';
 import { Container, Icon, SectionTitle } from '@/shared/components';
 import donation from '../../data/section-content.json';
 import s from './AboutPageMakeDonation.module.scss';
-import LinkButton from '@/shared/components/LinkButton/LinkButton.jsx';
-import { LINKDATA, ROUTES } from '@/shared/constants/index.js';
+
+import LinkButtonAboutPageMakeDonate from '../LinkButtonAboutPageMakeDonate/LinkButtonAboutPageMakeDonate.jsx';
 
 const AboutPageMakeDonation = () => {
-  const pathname = usePathname();
-  const locale = pathname.split('/')[1];
-
   return (
     <section className={s.makeDonationSection}>
       <Container>
@@ -20,14 +14,7 @@ const AboutPageMakeDonation = () => {
               <SectionTitle title={donation.title} />
               <p className={s.desc}>{donation.desc}</p>
             </div>
-            <div className={s.buttonsContainer}>
-              <LinkButton
-                path={`/${locale}/${ROUTES.PAYMENTS(1)}`}
-                linkText={donation.button_donate}
-                type={LINKDATA.TYPE_DARK_BF}
-                className={s.buttonSize}
-              />
-            </div>
+            <LinkButtonAboutPageMakeDonate />
           </div>
           {donation.image && (
             <div className={s.imageContainer}>

c5e05ee0e07aaa2bcd40e3c3159e730190cb3de3
Ella-Kadriaieva <ella.kadriaieva@gmail.com>
2025-02-23 08:55:35 +0200

    create const and refactored code

--- src/shared/helpers/getDonorText.js
+++ src/shared/helpers/getDonorText.js
@@ -1,23 +1,24 @@
+import { donorsDict } from '../constants';
 export const getDonorText = (count, locale) => {
   if (locale === 'ua') {
     const lastDigit = count % 10;
     const lastTwoDigits = count % 100;
 
     if (lastDigit === 1 && lastTwoDigits !== 11) {
-      return 'донор';
+      return donorsDict.ua[1];
     } else if (
       (lastDigit === 2 || lastDigit === 3 || lastDigit === 4) &&
       !(lastTwoDigits >= 11 && lastTwoDigits <= 14)
     ) {
-      return 'донори';
+      return donorsDict.ua[2];
     } else {
-      return 'донорів';
+      return donorsDict.ua[3];
     }
   } else if (locale === 'en') {
     if (count === 1) {
-      return 'donor';
+      return donorsDict.en[1];
     } else {
-      return 'donors';
+      return donorsDict.en[2];
     }
   }
 };

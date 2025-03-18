29b8e202e1303e1f38d5905643836f2776812405
z9877969 <z9877969@ukr.net>
2025-03-18 16:37:22 +0200

    changes serevr url for saveFileToUploadDir

--- src/app/server/lib/saveFileToUploadDir.js
+++ src/app/server/lib/saveFileToUploadDir.js
@@ -1,6 +1,7 @@
 import fs from 'node:fs/promises';
 import path from 'node:path';
-import env from '../utils/evn.js';
+// import env from '../utils/evn.js';
+import { insideServerApi } from '@/shared/services';
 
 export const TEMPLATES_DIR = path.join(process.cwd(), 'src', 'templates');
 export const UPLOAD_DIR = path.join(process.cwd(), 'uploads');
@@ -10,10 +11,10 @@ const saveFileToUploadDir = async (file) => {
   const tempPath = path.join(TEMP_UPLOAD_DIR, file.filename);
   const uploadPath = path.join(UPLOAD_DIR, file.filename);
 
-
   await fs.rename(tempPath, uploadPath);
 
-  return `${env('APP_DOMAIN')}/uploads/${file.filename}`;
+  // return `${env('APP_DOMAIN')}/uploads/${file.filename}`;
+  return `${insideServerApi.serverUrl}/public/images/all/${file.filename}`;
 };
 
 export default saveFileToUploadDir;
